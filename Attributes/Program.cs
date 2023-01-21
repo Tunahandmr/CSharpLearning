b36890b05d192afb7381553c53cffac449f87e91
Tunahan Demir <tunahandemir818@gmail.com>
2023-01-21 22:52:51 +0300

    Attribute and Reflection Added

---
+++ Attributes/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { Id=1,City="Trabzon",Name="Tunahan"};
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+            Console.ReadLine();
+        }
+    }
+
+    [ToTable("Customers")] // customers tablosuna karşılık gelir
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [RequiredProperty]
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+
+    class CustomerDal
+    {
+        [Obsolete("Add'i kullanma, AddNew'i kullan")]//hazır attribute, uyarı verir.
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} added!!",
+                customer.Id,customer.Name,customer.LastName,customer.City);
+        }
+
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} added!!",
+                customer.Id, customer.Name, customer.LastName, customer.City);
+        }
+    }
+
+    // [AttributeUsage(AttributeTargets.All)] bu attribute herkes kullanabilir
+    // [AttributeUsage(AttributeTargets.Class)] bu attribute sınıflar kullanabilir
+    //[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)] bu attribute property ve field'lar kullanabilir
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple =true)]// AllowMultiple =true -> attribute ard arda kullanilabilir 
+    class RequiredPropertyAttribute :Attribute 
+    {
+
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    class ToTableAttribute : Attribute
+    {
+        private string _tableName;
+
+        public ToTableAttribute(string tableName)
+        {
+            _tableName = tableName;
+        }
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* DortIslem dortIslem = new DortIslem(2, 3);
+             Console.WriteLine(dortIslem.Topla2());
+             Console.WriteLine(dortIslem.Topla(3, 4));*/
+
+            var tip = typeof(DortIslem);
+            /*
+            DortIslem dortIslem = (DortIslem)Activator.CreateInstance(tip,6,7);
+            Console.WriteLine(dortIslem.Topla2());
+            Console.WriteLine(dortIslem.Topla(1,2));*/
+
+            var instance = Activator.CreateInstance(tip, 6, 5);
+            MethodInfo methodInfo = instance.GetType().GetMethod("Topla2");
+            Console.WriteLine(methodInfo.Invoke(instance, null));
+
+            Console.WriteLine("----------------------");
+
+            var methodlar = tip.GetMethods();
+            foreach (var info in methodlar)
+            {
+                Console.WriteLine("Method Adı: " + info.Name);
+                foreach (var parameterInfo in info.GetParameters())
+                {
+                    Console.WriteLine("Parametreler: "+parameterInfo.Name);
+                }
+
+                foreach (var attribute in info.GetCustomAttributes())
+                {
+                    Console.WriteLine("Attributeler: "+attribute.GetType().Name);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class DortIslem
+    {
+        private int _x;
+        private int _y;
+
+        public DortIslem(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public int Topla(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Carp(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Topla2()
+        {
+            return _x + _y;
+        }
+
+        [MethodName("Carpma")]
+        public int Carp2()
+        {
+            return _x * _y;
+        }
+    }
+
+    public class MethodNameAttribute:Attribute
+    {
+        public MethodNameAttribute(string name)
+        {
+
+        }
+    }
+}
