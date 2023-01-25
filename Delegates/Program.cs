e7357e7634bb58025c71273052d8ba144478128d
Tunahan Demir <tunahandemir818@gmail.com>
2023-01-25 22:25:07 +0300

    Delegates and events added

---
+++ Delegates/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    // void ve parametresi olmayan metodlara elçilik yapar.
+    public delegate void MyDelegate();
+    // void ve string parametresi olan metodlara elçilik yapar.
+    public delegate void MyDelegate2(string text);
+
+    public delegate int MyDelegate3(int number1, int number2);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            // customerManager.SendMessage();
+            // customerManager.ShowAlert();
+
+            //delegate ile yapacağımız işleri sıralayabiliyoruz.
+            MyDelegate myDelegate = customerManager.SendMessage;
+            myDelegate += customerManager.ShowAlert; // ShowAlert'ı sıraya ekledik
+            myDelegate -= customerManager.SendMessage; // SendMessage'ı çıkardık 
+            myDelegate();
+
+            MyDelegate2 myDelegate2 = customerManager.SendMessage2;
+            myDelegate2 += customerManager.ShowAlert2;
+            myDelegate2("hello");// ikisine de aynı değeri yolluyor
+
+            Matematik matematik = new Matematik();
+            MyDelegate3 myDelegate3 = matematik.Topla;
+            myDelegate3+= matematik.Carp;
+            var sonuc = myDelegate3(2,5); // çıktı-> 10
+            // return olduğunda son işlem sonucu delege edilir.
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+        }
+    }
+
+    public class CustomerManager
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        public void ShowAlert()
+        {
+            Console.WriteLine("Be careful!");
+        }
+
+        public void SendMessage2(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void ShowAlert2(string alert)
+        {
+            Console.WriteLine(alert);
+        }
+    }
+
+    public class Matematik
+    {
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+    }
+}
--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -10,6 +10,41 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
+            //TryCatch();
+
+            //ActionDemo();
+
+            Func<int, int, int> add = Topla;
+            Console.WriteLine(add(2, 3));
+
+            Func<int> getRandomNumber = delegate ()
+            {
+                Random random = new Random();
+                return random.Next(1, 100);
+            };
+
+            Console.WriteLine(getRandomNumber());
+
+            Func<int> getRandomNumber2 = ()=> new Random().Next(1, 100);
+            Console.WriteLine(getRandomNumber2());
+            Console.ReadLine();
+        }
+
+        static int Topla(int x, int y)
+        {
+            return x + y;
+        }
+
+        private static void ActionDemo()
+        {
+            HandleException(() =>
+            {
+                Find();
+            });
+        }
+
+        private static void TryCatch()
+        {
             try
             {
                 string[] dizi = new string[2] { "Ahmet", "Hasan" };
@@ -19,8 +54,33 @@ namespace Exceptions
             {
                 Console.WriteLine(exception.Message);
             }
-
-            Console.ReadLine();
         }
+
+        private static void HandleException(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+
+            }
+        }
+
+        private static void Find()
+        {
+            List<string> student = new List<string> { "Tunahan", "Ali", "Hasan" };
+            if (!student.Contains("Ahmet"))
+            {
+                throw new RecordNotFoundException("Record not found!");
+            }
+            else
+            {
+                Console.WriteLine("Record found");
+            }
+        }
+
     }
 }
